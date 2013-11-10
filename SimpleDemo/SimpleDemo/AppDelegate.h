a2082634bbe4a1cd0c725f45114832a235078dd6
Mattias Levin <mattias.levin@gmail.com>
2013-11-10 11:26:39 +0100

    Added a simple demo project

---
+++ SimpleDemo/SimpleDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleDemo
+//
+//  Created by Mattias Levin on 10/11/13.
+//  Copyright (c) 2013 Mattias Levin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
