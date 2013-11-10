d2ca40e8e61aa588060a4789c1979914a1d7e4f8
Mattias Levin <mattias.levin@gmail.com>
2013-11-10 11:28:35 +0100

    Added a demo project for testing performance aspects

---
+++ PerformanceDemo/PerformanceDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PerformanceDemo
+//
+//  Created by Mattias Levin on 10/11/13.
+//  Copyright (c) 2013 Mattias Levin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PerformanceDemo/PerformanceDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PerformanceDemo
+//
+//  Created by Mattias Levin on 10/11/13.
+//  Copyright (c) 2013 Mattias Levin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
